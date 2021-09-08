e60fb2bed25f6d48511b859e7a86255df0105adb
StefanNijssen <58977715+StefanNijssen@users.noreply.github.com>
2021-09-08 09:26:12 +0200

    min en max

--- kaas.py
+++ kaas.py
@@ -4,8 +4,12 @@ b = input("waarde van b")
 if a > b:
     print("a is het grootste getal")
     max = a
+    min = b
 elif b > a:
     print("a is het kleinste getal")
     min = a
+    max = b
 else:
     print("a en b zijn even groot")
+print("Het minimum is: " + min)
+print("Het maximum is: " + max)
